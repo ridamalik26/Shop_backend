096dd16e60997fe78477cfb0aa0ac733cdb7d3d4
ridamalik26 <malikrida406@gmail.com>
2025-12-12 02:24:13 +0500

    get categories by categoryName

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const mongoose = require('mongoose');
 const authRouter = require('./routes/auth');
 const bannerRouter = require('./routes/banner');
 const categoryRouter = require('./routes/category');
+const subCategoryRouter = require('./routes/sub_category');
 //Define the port number the server will listen on
 const PORT = 2000;
 
@@ -19,6 +20,7 @@ app.use(express.json());
 app.use(authRouter);
 app.use(bannerRouter);
 app.use(categoryRouter);
+app.use(subCategoryRouter);
 
 mongoose.connect(DB).then(()=>{
     console.log('Mongodb Connected');
