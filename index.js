2edf9762f6b376c0af50d77355dac5cccab4028d
ridamalik26 <malikrida406@gmail.com>
2025-12-13 01:51:17 +0500

    popular product api

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const authRouter = require('./routes/auth');
 const bannerRouter = require('./routes/banner');
 const categoryRouter = require('./routes/category');
 const subCategoryRouter = require('./routes/sub_category');
+const productRouter = require('./routes/product');
 //Define the port number the server will listen on
 const PORT = 2000;
 
@@ -21,6 +22,7 @@ app.use(authRouter);
 app.use(bannerRouter);
 app.use(categoryRouter);
 app.use(subCategoryRouter);
+app.use(productRouter);
 
 mongoose.connect(DB).then(()=>{
     console.log('Mongodb Connected');
