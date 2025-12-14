430f64f53524b81cd47a5df508812601296efb79
ridamalik26 <malikrida406@gmail.com>
2025-12-14 17:47:04 +0500

    added cors

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const categoryRouter = require('./routes/category');
 const subCategoryRouter = require('./routes/sub_category');
 const productRouter = require('./routes/product');
 const productReviewRouter = require('./routes/product_review');
+const cors = require('cors');
 //Define the port number the server will listen on
 const PORT = 2000;
 
@@ -19,6 +20,7 @@ const DB = "mongodb+srv://malikrida406:BXpc6GlweqvJCDXU@cluster0.btmmmqd.mongodb
 //middleware - to register routes or to mount routes
 
 app.use(express.json());
+app.use(cors());
 app.use(authRouter);
 app.use(bannerRouter);
 app.use(categoryRouter);
