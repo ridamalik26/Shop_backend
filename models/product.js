1a6365dd30df8a5a3a85b794e5afcabbcd6625d9
ridamalik26 <malikrida406@gmail.com>
2026-06-17 22:46:35 +0500

    claculated total rating and average rating

--- models/product.js
+++ models/product.js
@@ -55,6 +55,15 @@ const productSchema = mongoose.Schema({
     recommend:{
         type:Boolean,
         default:false,
+    },
+    //Add these fields for ratings
+    averageRating:{
+        type:Number,
+        default:0,
+    },
+    totalRatings:{
+        type:Number,
+        default:0
     }
 
 });
--- routes/product_review.js
+++ routes/product_review.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const ProductReview = require('../models/product_review');
+const Product = require('../models/product');
 
 const productReviewRouter = express.Router();
 
@@ -22,6 +23,18 @@ productReviewRouter.post('/api/product-review', async (req, res) => {
             review
         });
         await reviews.save();
+
+        //find the product assoicated with the review using the productId
+        const product = await Product.findById(productId);
+        //if the product was not found, return a 404 error response
+        if(!product){
+            return res.status(404).json({msg: "product not found"});
+        }
+        product.totalRatings += 1;
+
+        product.averageRating = ((product.averageRating * (product.totalRatings - 1)) + rating) / product.totalRatings;
+
+        await product.save()
 
         return res.status(201).send(reviews);
     } catch (e) {
